399cc201d9d53a5a16e17fdb33681e8bc143696a
Frederic Blum <frederic_blum@eva.mpg.de>
2025-03-18 13:36:21 +0100

    first upload

--- raw/preprocessing/import.py
+++ raw/preprocessing/import.py
@@ -1,6 +1,6 @@
 import csv
-from lingpy import Wordlist
 from collections import defaultdict
+from lingpy import Wordlist
 
 datasets = [
     'blumpanotacana',
@@ -8,18 +8,20 @@ datasets = [
     'girardprototakanan',
     'valenzuelazariquieypanotakana'
     ]
+
 visited = []
 language_table = defaultdict()
 concept_table = defaultdict()
-formtable = defaultdict()
+form_table = defaultdict()
 
 for ds in datasets:
     wl = Wordlist.from_cldf(
         'cldf-resources/' + ds + '/cldf/cldf-metadata.json',
         columns=(
-            'parameter_id', 'concept_name', 'concept_concepticon_id', 'concept_concepticon_gloss', 'concept_proto_id',
-            'language_id', 'language_name', 'language_family', 'language_glottocode',
-            'value', 'form', 'segments', 'cognacy'
+            'id', 'parameter_id', 'concept_name', 'concept_concepticon_id', 'concept_concepticon_gloss',
+            'concept_proto_id', 'language_id', 'language_name', 'language_family',
+            'language_glottocode', 'value', 'form', 'segments', 'cognacy', 'partial_cognacy',
+            'comment', 'alignment', 'source', 'morphemes', 'borrowing'
             ),
         namespace=(
             ('concept_name', 'concept'),
@@ -33,38 +35,69 @@ for ds in datasets:
             )
         )
 
-    for item in wl:
+    for i, item in enumerate(wl):
         # Add languages
-        checkup = [wl[item, 'language_name'], ds]
-        if checkup not in visited and wl[item, 'language_family'] == 'Pano-Tacanan':
-            visited.append(checkup)
+        checkup_concept = [wl[item, 'language_name'], ds]
+        language_id = wl[item, 'doculect'] + '_' + ds
+        if checkup_concept not in visited and wl[item, 'language_family'] == 'Pano-Tacanan':
+            visited.append(checkup_concept)
             language_table[wl[item, 'language_name']] = [
-                wl[item, 'doculect'],
+                language_id,
                 wl[item, 'glottolog'],
                 ds
                 ]
 
         # Add concepts
-        if wl[item, 'parameter_id'] not in concept_table:
-            concept_table[wl[item, 'parameter_id']] = [
+        concept_id = wl[item, 'parameter_id'] + '_' + ds
+        if concept_id not in concept_table:
+            concept_table[concept_id] = [
+                wl[item, 'concept'],
                 wl[item, 'concepticon_gloss'],
                 wl[item, 'concepticon'],
                 wl[item, 'concept_proto_id']
                 ]
 
+        form_table[i] = [
+            wl[item, 'id'],
+            language_id,
+            concept_id,
+            wl[item, 'value'],
+            wl[item, 'form'],
+            wl[item, 'tokens'],
+            wl[item, 'comment'],
+            wl[item, 'source'],
+            wl[item, 'cognacy'],
+            wl[item, 'partial_cognacy'],
+            wl[item, 'alignment'],
+            wl[item, 'morphemes'],
+            wl[item, 'borrowing'],
+            ds
+        ]
+
+
 for item in concept_table:
     print(item, concept_table[item])
 
-with open('languages.tsv', 'w', newline='', encoding='utf8') as csvfile:
+language_table = dict(sorted(language_table.items(), key=lambda item: item[1][1]))
+
+with open('../../etc/languages.tsv', 'w', newline='', encoding='utf8') as csvfile:
     writer = csv.writer(csvfile, delimiter='\t')
     writer.writerow(['Name', 'ID', 'Glottocode', 'Dataset'])
 
     for key, values in language_table.items():
         writer.writerow([key] + values)
 
-with open('concepts.tsv', 'w', newline='', encoding='utf8') as csvfile:
+concept_table = dict(sorted(concept_table.items(), key=lambda item: item[1][0]))
+with open('../../etc/concepts.tsv', 'w', newline='', encoding='utf8') as csvfile:
     writer = csv.writer(csvfile, delimiter='\t')
-    writer.writerow(['Parameter_ID', 'Concepticon_Gloss', 'Concepticon_ID', 'Proto_ID'])
+    writer.writerow(['Parameter_ID', 'Concept', 'Concepticon_Gloss', 'Concepticon_ID', 'Proto_ID'])
 
     for key, values in concept_table.items():
+        writer.writerow([key] + values)
+
+with open('../../raw/raw.tsv', 'w', newline='', encoding='utf8') as csvfile:
+    writer = csv.writer(csvfile, delimiter='\t')
+    writer.writerow(['ID', 'Form_ID', 'Language_ID', 'Parameter_ID', 'Value', 'Form', 'Segments', 'Comment', 'Source', 'Cognacy', 'Partial_Cognacy', 'Alignment', 'Morphemes', 'Borrowing', 'Dataset'])
+
+    for key, values in form_table.items():
         writer.writerow([key] + values)
